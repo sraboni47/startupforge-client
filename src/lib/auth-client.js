c0b9eded5cb40e1f555c1e2080026581c96efa73
sraboni47 <sarkersraboni47@gmail.com>
2026-06-30 19:59:27 +0600

    Update auth client baseURL

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://startupforge-server-5pdk.vercel.app",
 });
